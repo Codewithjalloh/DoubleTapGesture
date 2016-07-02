96e5995a2b2a3655d93d40fac75dcbe378d52e1c
D.A Jalloh <info@codewithjalloh.com>
2016-07-02 23:17:00 +0100

    outlet connected

--- myDoubleTapGesture/ViewController.swift
+++ myDoubleTapGesture/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
