84e5dcc24f1d0d1c7a8a0011afa1989b7c47a105
D.A Jalloh <info@codewithjalloh.com>
2016-07-02 23:27:21 +0100

    code complete

--- myDoubleTapGesture/ViewController.swift
+++ myDoubleTapGesture/ViewController.swift
@@ -17,13 +17,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        tapGesture.numberOfTapsRequired = 2
+        imageView.addGestureRecognizer(tapGesture)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func handleTap(sender: UIGestureRecognizer) {
+        if (sender.view?.contentMode == .ScaleAspectFit) {
+            sender.view?.contentMode = .Center
+        } else {
+            sender.view?.contentMode = .ScaleAspectFit
+        }
+    }
+    
+    
 
 
 }
